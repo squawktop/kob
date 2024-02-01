597cf0c19b4ce6d86464d489a9ff514475b4b490
Huai Xue <1103356347@qq.com>
2024-02-01 20:22:43 +0800

    实现了导航栏 + 对战页面的地图和障碍物

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,49 +1,29 @@
 <template>
-  <div>
-    <div>Bot昵称：{{bot_name}}</div>
-    <div>Bot战力：{{ bot_rating }}</div>
-  </div>
+  <NavBar />
   <router-view></router-view>
 </template>
 
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
-
-
+import NavBar from "@/components/NavBar.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 export default {
-  name: "App",
-  setup: () => {
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        // console.log(resp)
-        bot_name.value = resp.name; 
-        bot_rating.value = resp.rating;
-      }
-    })
-    let bot_name = ref("");
-    let bot_rating = ref("");
-    return {
-      bot_name,
-      bot_rating
-    }
-  }
-}
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 
 
 <style>
-body{
-  margin:0px;
-  background: url("@/assets/background.jpg") no-repeat;
-  background-size:100% 100%;
-  background-attachment:fixed;
+body {
+  margin: 0px;
+  background: url(" @/assets/images/background.jpg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
 
   /* background-image: url("@/assets/background.jpg");
   background-repeat: no-repeat;
