101983e5c26866087debe69c6195a1a2e204bda3
Huai Xue <1103356347@qq.com>
2024-02-07 19:27:06 +0800

    5.1 创建个人中心页面（上）

---
+++ backend/src/main/java/com/kob/backend/controller/user/bot/AddController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.user.bot;
+
+import com.kob.backend.service.user.bot.AddService;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AddController {
+    @Autowired
+    private AddService addService ;
+
+    @PostMapping("/user/bot/add/") //添加操作是需要修改数据库的，推荐使用post
+    public Map<String , String> add(@RequestParam Map<String , String> data){
+        return addService.add(data);
+    }
+}
