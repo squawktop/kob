8a337c9f89590a12a1322e264095799d13c9bb30
Huai Xue <1103356347@qq.com>
2024-02-24 17:50:58 +0800

    10.1 实现AcApp端（上）

--- acapp/src/App.vue
+++ acapp/src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
+  <router-view></router-view>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "@/components/NavBar.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+// import $ from "jquery";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+  },
+  setup() {
+    // 发送 POST 请求
+
+
+    //   $.ajax({
+    //     url: "http://127.0.0.1:3000/api/user/account/info/",
+    //     type: "get",
+    //     headers: {
+    //       Authorization:
+    //         "Bearer " +
+    //         "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYzVkYjdmM2Q1MjU0ZDFlOWIzZTVkNzRiMzhhMDkzMCIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTcwNzExNTE1MCwiZXhwIjoxNzA4MzI0NzUwfQ.FRtWhMYr6wIGWPJmzFA8krKkrjYpcJka_l5P7ox4u44",
+    //     },
+    //     success(resp) {
+    //       console.log(resp);
+    //     },
+    //     error(resp) {
+    //       console.log(resp);
+    //     },
+    //   });
+
+    //   $.ajax({
+    //     url: "http://127.0.0.1:3000/api/user/account/register/",
+    //     type: "post",
+    //     data: {
+    //       username: "yxc2",
+    //       password: "123",
+    //       confirmedPassword: "123",
+    //     },
+    //     success(resp) {
+    //       console.log(resp);
+    //     },
+    //     error(resp) {
+    //       console.log(resp);
+    //     },
+    //   });
+  },
+};
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0px;
+  background: url(" @/assets/images/background.jpg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+
+  /* background-image: url("@/assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover; */
 }
 </style>
---
+++ acapp/src/router/index.js
@@ -0,0 +1,101 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '@/views/pk/PkIndexView'
+import RecordIndexView from '@/views/record/RecordIndexView'
+import RecordContentView from '@/views/record/RecordContentView'
+import RanklistIndexView from '@/views/ranklist/RanklistIndexView'
+import UserBotIndexView from '@/views/user/bot/UserBotIndexView'
+import NotFound from '@/views/error/NotFound'
+import UserAccountLoginView from "@/views/user/account/UserAccountLoginView"
+import UserAccountRegisterView from "@/views/user/account/UserAccountRegisterView"
+import store from '@/store/index'
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+    meta: { //该页面是否需要授权      //随便写， 也可以设置为   flag : true;
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+    meta: { //该页面是否需要授权
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+    meta: { //该页面是否需要授权
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/record/:recordId/",
+    name: "record_content",
+    component: RecordContentView,
+    meta: { //该页面是否需要授权
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+    meta: { //该页面是否需要授权
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+    meta: { //该页面是否需要授权
+      requestAuth: true,
+    }
+  },
+  {
+    path: "/user/account/login/",
+    name: "user_account_login",
+    component: UserAccountLoginView,
+    meta: { //该页面是否需要授权
+      requestAuth: false,
+    }
+  },
+  {
+    path: "/user/account/register/",
+    name: "user_account_register",
+    component: UserAccountRegisterView,
+    meta: { //该页面是否需要授权
+      requestAuth: false,
+    }
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",  //页面重定向到404
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requestAuth && !store.state.user.is_login) {
+    next({ name: "user_account_login" });
+  } else {
+    next();
+  }
+})
+
+export default router
